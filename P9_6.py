f6570f9b9b78514fe05223fc257594fd5348fd21
Ddd <752042257@qq.com>
2019-02-13 20:23:32 +0800

    day1

--- P9_6.py
+++ P9_6.py
@@ -7,7 +7,7 @@ y_data = x_data * 0.1 + 0.3
 
 ###create tensorflow structure start##$#
 Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-biases = tf.Variable(tf.zero([1]))
+biases = tf.Variable(tf.zeros([1]))
 
 y = Weights * x_data + biases
 
@@ -20,4 +20,9 @@ init = tf.global_variables_initializer()
 
 
 sess = tf.Session()
-sess.run(init)    
+sess.run(init)  # Vary important
+
+for step in range(201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(Weights), sess.run(biases))
